bf7e92086fb2c530f083725a8324fa303208d347
salehloke <salehloke@gmail.com>
2022-08-23 17:26:54 +0800

    4 files modified, 2 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BasicFormComponent } from './basic-form/basic-form.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HelloComponent,
+    component: AppComponent,
+  },
+  {
+    path: 'basic-form',
+    component: BasicFormComponent,
   },
 ];
 
