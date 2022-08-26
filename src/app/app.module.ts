b7fbc97e55c0147f5e510608862a832c36a292b0
salehloke <salehloke@gmail.com>
2022-08-26 16:12:03 +0800

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { BasicFormComponent } from './basic-form/basic-form.component';
+import { ModalFormComponent } from './modal-form/modal-form.component';
 
 @NgModule({
   imports: [
@@ -14,10 +17,11 @@ import { BasicFormComponent } from './basic-form/basic-form.component';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    NgbModule,
     AppRoutingModule,
     ToastrModule.forRoot(),
   ],
-  declarations: [AppComponent, BasicFormComponent],
+  declarations: [AppComponent, BasicFormComponent, ModalFormComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
