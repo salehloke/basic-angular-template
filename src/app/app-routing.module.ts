b99a10908d907cb20d26cc8eb0611ba9ea73f106
salehloke <salehloke@gmail.com>
2022-08-26 16:17:55 +0800

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BasicFormComponent } from './basic-form/basic-form.component';
+import { ModalFormComponent } from './modal-form/modal-form.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BasicFormComponent,
+  },
+  {
+    path: 'modal-form',
+    component: ModalFormComponent,
   },
 ];
 
